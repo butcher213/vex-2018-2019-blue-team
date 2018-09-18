869edfc4faf90d5029e99e597dad4fc278f5d3ab
unknown <trevordbutcher@gmail.com>
2018-09-18 15:12:08 -0700

    added Sensors.h and Sensors.c to each robot's source files

---
+++ .vscode/cquery_cached_index/d@@vex 2018@vex-2018-2019-blue-team/capper@src@sensors.c
@@ -0,0 +1,19 @@
+#include "Sensors.h"
+#include "main.h"
+/* Function:		moveIn
+ * Purpose:			moves the robot a specified amount of inches
+ * Argument:		inches = amount of inches to move
+ * Returns:			N/A
+ */
+void moveIn(float inches) {
+
+
+}
+/* Function:		rotateTo
+ * Purpose:			rotates the robot to the specified degree
+ * Argument:		deg = degree to move to
+ * Return:			N/A
+ */
+void rotateTo(float targetDeg) {
+
+}
