b12ea51e4836b32fe5828a173f92c17046a8f60e
unknown <trevordbutcher@gmail.com>
2018-10-01 17:02:45 -0700

    Updated PID in sensors.c to

--- shooter/include/Sensors.c
+++ shooter/include/Sensors.c
@@ -6,24 +6,32 @@
  * Returns:			N/A
  */
  #define PI 3.1415
-void moveIn(float inches) {
-  double rightpos = 0;
-  double target = inches * PI * 4;
+void moveIn(int inches) {
   motor_set_zero_position(2,0);
-  while(rightpos / 360 < 12 * PI * 4) {
+  int speed = 30;
+  double rightpos = 0;
+  while(rightpos < (inches * (360 / (PI * 2)))) {
+    /*if(rightpos > (.9 * 12 * (360/(PI * 4)))) {
+      speed = - 500 * inches * (rightpos - inches);
+    }*/
     rightpos =  motor_get_position(2);
-    printf("value: %.2f\n", rightpos);
+    printf("value: %f\n", motor_get_actual_velocity(2));
     double leftpos = motor_get_position(1);
     if(leftpos > rightpos) {
-      motor_move(2, 10);
-      motor_move(1,10 * .9);
+      motor_move(2, speed);
+      motor_move(1,speed * .9);
     } else {
-      motor_move(2, 10);
-      motor_move(1,10 * .9);
+      motor_move(2, speed);
+      motor_move(1,speed * .9);
     }
   }
-  motor_move(2,0);
-  motor_move(1,0);
+  motor_set_zero_position(2,0);
+  while( 0 - motor_get_actual_velocity(2) > 0) {
+    motor_move(2,-50);
+    motor_move(1,-50);
+  }
+  motor_move(1, 0);
+  motor_move(2, 0);
 }
 /* Function:		rotateTo
  * Purpose:			rotates the robot to the specified degree
@@ -32,4 +40,31 @@ void moveIn(float inches) {
  */
 void rotateTo(float targetDeg) {
 
+}
+void PID_control() {
+  float K_p = .5;
+  float K_i = 0;
+  float K_d = 0;
+  double integral_left = 0;
+  double integral_right = 0;
+  float prev_error_left = 0;
+  float prev_error_right = 0;
+  while(1) {
+    float error_left = pid_target - motor_get_position(1);
+    float error_right = pid_target - motor_get_position(2);
+    integral_left += error_left;
+    integral_right += error_right;
+    if (error_left == 0 || abs(error_left) > 40){
+      integral_left = 0;
+    }
+    if (error_right == 0 || abs(error_right) > 40){
+      integral_right = 0;
+    }
+    double deriv_left = error_left - prev_error_left;
+    prev_error_left = error_left;
+    pid_speed_left = K_p * error_left + K_i * integral_left + K_d * deriv_left;
+    double deriv_right = error_right - prev_error_right;
+    prev_error_right = error_right;
+    pid_speed_right = K_p * error_right + K_i * integral_right + K_d * deriv_right;
+  }
 }
