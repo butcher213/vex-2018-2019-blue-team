e88fc21d8b89d619714c3cfdbdc35f80c385606f
Katherine Rocha <43391313+Shazner@users.noreply.github.com>
2018-10-02 15:11:11 -0700

    idk what this is

---
+++ .vscode/cquery_cached_index/k@@krocha@documents@github@vex-2018-2019-blue-team/include@pid.c
@@ -0,0 +1,33 @@
+#include "PID.h"
+#include "math.h"
+
+int updatePID(PID_properties_t *prop) {
+	int speed, i;
+	
+	prop->error = prop->target - motor_get_position(prop->motorPorts[0]);
+	prop->integral += prop->error;
+
+	if (prop->error == 0)
+		prop->integral = 0;
+	if (abs(prop->error) > prop->startSlowingValue)
+		prop->integral = 0;
+
+	prop->derivative = prop->error - prop->previousError;
+	prop->previousError = prop->error;
+
+	speed = prop->Kp * prop->error + prop->Ki * prop->integral + prop->Kd * prop->derivative;
+	
+	for (i = 0; i < prop->numMotorPorts; ++i)
+		motor_move(prop->motorPorts[i], speed);
+
+	return speed;
+}
+
+PID_properties_t *createPID(int *motorPorts, int startSlowingValue) {
+	PID_properties_t *prop;
+
+	prop->motorPorts = motorPorts;
+	prop->startSlowingValue = startSlowingValue;
+
+	return prop;
+}
